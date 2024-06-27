0b23f08ab58a21f76e7f6f6c0e174381f0c568b9
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-06-27 18:29:12 +0000

    priority var changed to match_priority

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,12 @@
 task = input("Enter your task: ")
-priority= input("Priority (high/medium/low): ")
+match_priority= input("Priority (high/medium/low): ")
 time= input("Is it time-bound? (yes/no): ")
 
-if priority == "high":
+if match_priority == "high":
     print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-elif priority == "low":
+elif match_priority == "low":
     print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-elif priority == "medium":
+elif match_priority == "medium":
     print(f"Note: '{task}' is a medium priority task. Consider completing it after a high priority.")
 else:
     print("Invalid level please use (high/medium/low)")
