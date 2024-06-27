6d5d441df188dd8c633ce3b29d7b16f41f3127de
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-06-27 18:09:40 +0000

    priority variable fixed

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,13 +1,13 @@
 task = input("Enter your task: ")
-priority_level = input("Priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 time_bond = input("Is it time-bound? (yes/no): ")
 print()
 
-if priority_level == "high":
+if priority == "high":
     print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-elif priority_level == "low":
+elif priority == "low":
     print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-elif priority_level == "medium":
+elif priority == "medium":
     print(f"Note: '{task}' is a medium priority task. Consider completing it after a high priority.")
 else:
     print("Invalid level please use (high/medium/low)")
