d484ad25fdd341b4c8323d6095923c0d09ea8d49
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-06-27 18:41:04 +0000

    used match case instead of if elif else

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,13 +1,15 @@
 task = input("Enter your task: ")
-match_priority = input("Priority (high/medium/low): ")
-time= input("Is it time-bound? (yes/no): ")
+priority = input("Priority (high/medium/low): ")
+time = input("Is it time-bound? (yes/no): ")
 
-if match_priority == "high":
-    print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-elif match_priority == "low":
-    print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-elif match_priority == "medium":
-    print(f"Note: '{task}' is a medium priority task. Consider completing it after a high priority.")
-else:
-    print("Invalid level please use (high/medium/low)")
+match priority:
+    case "high":
+        print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+    case "low":
+        print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
+    case _:
+        print("Invalid level please use (high/medium/low)")
+
+
+
     
