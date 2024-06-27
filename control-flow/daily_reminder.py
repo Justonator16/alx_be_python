e03e78b2e0a3934b1c7a0aff302f9ff8e39721cb
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-06-27 18:46:53 +0000

    time var changed to time_bound

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,13 +2,14 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match priority:
-    case "high":
-        print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-    case "low":
-        print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-    case _:
-        print("Invalid level please use (high/medium/low)")
+if time_bound == "yes":
+    match priority:
+        case "high":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+        case "low":
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
+        case _:
+            print("Invalid level please use (high/medium/low)")
 
 
 
