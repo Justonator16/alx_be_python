0e7842a69eab5f7ab5b41eb84e1fcccc332f22a5
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-03 12:09:57 +0000

    temperature  validation

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 9/5
-CELSIUS_TO_FAHRENHEIT_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 5 / 9
 
 # Formular (x - 32) x (5/9) where x is fahrenheit
 def convert_to_celsius(fahrenheit):
