565ff10f3cc381f07e5ad5885243205bd1affbee
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-03 12:07:18 +0000

    temperature  validation

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 9/5
-CELSIUS_TO_FAHRENHEIT_FACTOR =  5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 5/9
 
 # Formular (x - 32) x (5/9) where x is fahrenheit
 def convert_to_celsius(fahrenheit):
@@ -11,14 +11,17 @@ def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * FAHRENHEIT_TO_CELSIUS_FACTOR) + 32
     return f"{celsius}°C is {fahrenheit} °F"
 
-temperature = int(input("Enter the temperature to convert: "))
+temperature = input("Enter the temperature to convert: ")
 type_of_temperature = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+if temperature.isnumeric() == False:
+    print("Invalid temperature. Please enter a numeric value.")
 
 if type_of_temperature == "F":
     print(convert_to_celsius(temperature))
 elif type_of_temperature == "C":
     print(convert_to_fahrenheit(temperature))
 else:
-    print("Invalid symbol enter C                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               1UUUor F")
+    print("Invalid symbol enter C or F")
 
 
