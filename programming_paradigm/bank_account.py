3e0bfcd3fd92d7c059c7efe863cf6d5feeab5771
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-08 15:07:13 +0000

    OOP practice

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+
+# deposit should add the specified amount to account_balance
+    def deposit(self ,amount):
+        self.account_balance += amount
+# withdraw should deduct the amount from account_balance if funds are sufficient, returning True; otherwise, return False and do not alter the balance.
+    def withdraw(self, amount):
+        deduction = self.account_balance - amount
+        if  deduction < 0:
+            return False
+        else:
+            return True
+
+# display_balance should print the current balance in a user-friendly format.
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
