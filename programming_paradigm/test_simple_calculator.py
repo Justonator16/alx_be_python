51a4c5069b4688e9ec4d83abcd4bd89da6249720
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-09 13:34:31 +0000

    function names fixed

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,9 +13,9 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3),2)
     
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,3),6)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4,0),None)
         self.assertEqual(self.calc.divide(4,2),2)
