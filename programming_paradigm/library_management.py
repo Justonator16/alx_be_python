675ef024e9c5e479380a79c5bfb30a74580f8ea5
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-10 18:48:03 +0000

    Library manager

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title = "", author = ""):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+class Library:     
+    def __init__(self, books = [], checkouts = []):
+        self.books = list(books)
+        self.checkouts = list(checkouts)
+
+    def add_book(self,title_and_book):
+        self.books.append(title_and_book)
+
+    def check_out_book(self, title):
+        # for i in range(len(self.books)):
+        #     if title in self.books[i]:
+        #         book = self.books[i]
+        #         self.checkouts.append(book)
+        #         self.books.pop(i)
+        self.checkouts.append(self.books[1])
+        self.books.pop(1) 
+
+    
+    def return_book(self, title):
+        # for i in range(len(self.checkouts)):
+        #     if title in self.checkouts[i]:
+        #         book = self.checkouts[i]
+
+        #         self.books.append(book)
+        #         self.checkouts.pop(i)
+
+        self.books.append(self.checkouts[0])
+        self.checkouts.pop(0)
+
+    def list_available_books(self):
+        for i in self.books:
+            print(i)
