40e7a0e8504b55459fc3b36b048bbcb7df74e8ea
Justonator16 <131472241+Justonator16@users.noreply.github.com>
2024-07-11 20:38:52 +0000

    Dict used instead of lists

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,7 +3,7 @@ class Test:
         pass
         
 class Book:
-    def __init__(self, title = "", author = ""):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
@@ -11,34 +11,27 @@ class Book:
         return f"{self.title} by {self.author}"
     
 class Library:     
-    def __init__(self, books = [], checkouts = []):
-        self._books = []
-        self.checkouts = []
+    def __init__(self, books = {}, checkouts = {}):
+        self.books = books
+        self.checkouts = checkouts
 
     def add_book(self,title_and_book):
-        self.books.append(title_and_book)
+        title_book_dict = {title_and_book.title: title_and_book.author}
+        self.books.update(title_book_dict)
 
     def check_out_book(self, title):
-        # for i in range(len(self.books)):
-        #     if title in self.books[i]:
-        #         book = self.books[i]
-        #         self.checkouts.append(book)
-        #         self.books.pop(i)
-        self.checkouts.append(self.books[1])
-        self.books.pop(1) 
-
+        if title in self.books:
+            title_book_dict = {title: self.books[title]}
+            self.checkouts.update(title_book_dict)
+            self.books.pop(title)
     
-    def return_book(self):
-        # for i in range(len(self.checkouts)):
-        #     if title in self.checkouts[i]:
-        #         book = self.checkouts[i]
-
-        #         self.books.append(book)
-        #         self.checkouts.pop(i)
-        if self.books == True:
-            self.books.append(self.checkouts[0])
-            self.checkouts.pop(0)
+    def return_book(self, title):
+        if title in self.checkouts:
+            title_book_dict = {title : self.checkouts[title] }
+            self.books.update(title_book_dict)
+            self.checkouts.pop(title)
 
     def list_available_books(self):
         for i in self.books:
-            print(i)
+            checker_text = f"{i} by {self.books[i]}"
+            print(checker_text)
